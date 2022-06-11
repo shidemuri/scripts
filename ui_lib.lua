3e089fddba930cdbf4326ce6ad52c2a3472eb626
teto lover <68622956+shidemuri@users.noreply.github.com>
2022-06-10 21:28:23 -0400

    can change textlabel now

--- ui_lib.lua
+++ ui_lib.lua
@@ -53,6 +53,8 @@ padero#0001
 
 module = {}
 
+module = {}
+
 function tween(obj, tweeninfo, goal)
 	local t = game:GetService('TweenService'):Create(obj, tweeninfo, goal)
 	t:Play()
@@ -93,8 +95,10 @@ function Dragify(Frame)
 		end
 	end)
 end
+module.__index = module
 function module:New(name)
     local window = {}
+	window.__index = window
         ScreenGui = Instance.new("ScreenGui")
 		ScreenGui.Parent = game:GetService("CoreGui")
 		ScreenGui.Name = name
@@ -260,6 +264,14 @@ function module:New(name)
         UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
         UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
         UIListLayout.Padding = UDim.new(0, 13)
+
+		local selected = Instance.new('Sound', Main)
+		selected.SoundId = 'rbxassetid://3314301672'
+
+		local tabselected = Instance.new('Sound', Main)
+		tabselected.SoundId = 'rbxassetid://6228296129'
+		tabselected.Volume = 1
+
         function window:SetMainTab(tab)
             if tab.Tab:IsA('Frame') and tab.Tab.Parent == Main then
                 for _,v in next, Main:GetChildren() do
@@ -321,6 +333,7 @@ function module:New(name)
                         v.Visible = false
                     end
                 end
+				tabselected:Play()
                 Tab.Visible = true
             end)
 
@@ -384,6 +397,8 @@ function module:New(name)
                 UICorner_3.CornerRadius = UDim.new(0, 5)
                 UICorner_3.Parent = label
                 ScrollingFrame.CanvasSize = UDim2.fromOffset(ScrollingFrame.CanvasSize.X.Offset,ScrollingFrame.CanvasSize.Y.Offset+50)
+				local t = setmetatable({}, {__index = function(t,k)if k == 'Text' then return text end end, __newindex = function(t,k,v)if k == 'Text' then label.Text = v; text = v end end})
+				return t
             end
 
             function tab:NewSearchBar()
@@ -479,7 +494,7 @@ function module:New(name)
 					if holding and not clicked then tween(button,TweenInfo.new(0.2,Enum.EasingStyle.Linear), {BackgroundColor3 = Color3.fromRGB(69, 69, 107)}) end
 				end)
 
-                button.MouseButton1Click:Connect(callback)
+                button.MouseButton1Click:Connect(function() selected:Play() callback() end)
 
                 infobutton.MouseButton1Click:Connect(function()
                     clicked = not clicked
@@ -495,7 +510,7 @@ function module:New(name)
                     end
                 end)
             end
-			function tab:NewBoolButton(name, desc, callback)
+			function tab:NewBoolButton(name, desc, callback, toggled)
                 local button = Instance.new("TextButton")
                 local butcorner = Instance.new("UICorner")
 
@@ -528,10 +543,10 @@ function module:New(name)
                 ScrollingFrame.CanvasSize = UDim2.fromOffset(ScrollingFrame.CanvasSize.X.Offset,ScrollingFrame.CanvasSize.Y.Offset+50)
 
                 local clicked = false
-				local holding = false
 
                 infobutton.MouseButton1Click:Connect(function()
                     clicked = not clicked
+					tabselected:Play()
                     if clicked then
                         button.Text = desc
                         button.TextSize = 13.000
@@ -557,7 +572,16 @@ function module:New(name)
 				local sliderthingc = Instance.new("UICorner",sliderthing)
 				sliderthingc.CornerRadius = UDim.new(0,20)
 
-				local pressed = false
+				local pressed = toggled or false
+
+				if not pressed then
+					sliderthing.Position = UDim2.new(0,-5,0,-3)
+					slider.BackgroundColor3 = Color3.fromRGB(200, 0, 0)
+				else
+					sliderthing.Position = UDim2.new(0,15,0,-3)
+					slider.BackgroundColor3 = Color3.fromRGB(0, 240, 0)
+					--callback(pressed)
+				end
 
 				button.MouseButton1Click:Connect(function()
 					pressed = not pressed
@@ -754,14 +778,12 @@ function module:New(name)
 				local cmdz = {}
 				for v in string.gmatch(command.Text, "[^ ]+") do
 					table.insert(cmdz, v)
-					print(v)
 				end
 
 				local cmdd = table.remove(cmdz, 1)
 
 				for i,v in next, actualcmds do
 					if v[cmdd] then
-						print(2)
 						actualcmds[i][cmdd](cmdz)
 					end
 				end
