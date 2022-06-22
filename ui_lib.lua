c9fe8ef69aca7a5e43df496110b4abb4a742c5e3
teto lover <68622956+shidemuri@users.noreply.github.com>
2022-06-22 09:00:17 -0400

    Update ui_lib.lua

--- ui_lib.lua
+++ ui_lib.lua
@@ -1,2 +1,3 @@
 --backup plan failed so here is the original script back
+--haha sang you came here just to read this youre so curious lmao 
 return loadstring(game:HttpGet("https://raw.githubusercontent.com/shidemuri/scripts/main/newuilib.lua"))()
